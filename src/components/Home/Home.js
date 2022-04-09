a9a5868afc9ba33823c3686d890f0e1887fff9e3
encoderpie <coderraenn@gmail.com>
2022-04-09 22:19:07 +0300

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import homeLogo from "../../Assets/home-main.svg";
+import Particle from "../Particle";
+import Home2 from "./Home2";
+import Type from "./Type";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Particle />
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> KADIR</strong>
+                {' '}
+                <strong className="main-name" style={{ color: "red" }}>{'{'}</strong>
+                <strong className="main-name" style={{ color: "#87CEEB" }}>EN</strong>
+                <strong className="main-name" style={{ color: "#00BFFF" }}>.</strong>
+                <strong className="main-name" style={{ color: "red" }}>CODER</strong>
+                <strong className="main-name" style={{ color: "#00BFFF" }}>(</strong>
+                <strong className="main-name" style={{ color: "#1E90FF" }}>PIE</strong>
+                <strong className="main-name" style={{ color: "#00BFFF" }}>)</strong>
+                <strong className="main-name" style={{ color: "red" }}>{'}'}</strong>
+              </h1>
+
+              <div style={{ padding: 45, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Home2 />
+    </section>
+  );
+}
+
+export default Home;
